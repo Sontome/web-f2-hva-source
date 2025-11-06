e1482482419eda648c4e7098bfef60e0736d26cc
Sontome <93179211+Sontome@users.noreply.github.com>
2025-11-06 16:50:47 +0700

    Update EmailTicketModal.tsx

--- src/components/EmailTicketModal.tsx
+++ src/components/EmailTicketModal.tsx
@@ -276,6 +276,7 @@ export const EmailTicketModal = ({ isOpen, onClose }: EmailTicketModalProps) => 
               onCheckedChange={handleSwitchChange}
             />
             <Label htmlFor="guiChung">{formData.guiChung ? 'Gửi chung' : 'Gửi riêng từng Pax'}</Label>
+            <div> 
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button type="button" variant="outline" className="min-w-[120px]">
@@ -294,6 +295,7 @@ export const EmailTicketModal = ({ isOpen, onClose }: EmailTicketModalProps) => 
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
+            </div> 
           </div>
           
           <div className="flex justify-end space-x-2 pt-4">
